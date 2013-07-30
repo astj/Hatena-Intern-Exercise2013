4ec1ad54bcbceecc1f9aecc991f4bbf9beadbc64
Asato Wakisaka <caaov627@hcn.zaq.ne.jp>
2013-07-31 00:44:44 +0900

    Q3古典的実装でとりあえずやっつけた

--- perl/lib/LogCounter.pm
+++ perl/lib/LogCounter.pm
@@ -8,9 +8,22 @@ sub new {
 };
 
 sub group_by_user {
+    my $self = shift;
+    my $groups = {};
+
+    #古典的実装
+    # @$self->{logs}だと @$selfでひとまとまりになっちゃうからダメなんですね
+    foreach (@{ $self->{logs} }) {
+        push(@{$groups->{$_->{user} // 'guest'}}, $_ );
+    }
+
+    return $groups;
 }
 
 sub count_error {
+    my $self = shift;
+
+    return scalar grep {$_->{status} > 499 && $_->{status} < 600} @{ $self->{logs} };
 }
 
 1;
