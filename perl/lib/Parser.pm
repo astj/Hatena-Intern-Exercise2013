4046109b93461cd4f6018413deafe9ce6a813d5f
Asato Wakisaka <caaov627@hcn.zaq.ne.jp>
2013-07-31 00:26:54 +0900

    Q2とりあえず

--- perl/lib/Parser.pm
+++ perl/lib/Parser.pm
@@ -1,6 +1,7 @@
 package Parser;
 use strict;
 use warnings;
+use Log;
 
 sub new {
     my ($class, %args) = @_;
@@ -8,6 +9,28 @@ sub new {
 }
 
 sub parse {
+    my $self = shift;
+
+    my $ResultArray = [];
+    open my $fh, '<', $self->{filename} or die $!;
+
+    # 1行ずつ読み込みますか
+    while(my $line = <$fh>) {
+        chomp $line;
+        my @SplittedLine = split("\t",$line);
+
+#        # 半古典的手法
+#        my $ParamsHash = {};
+#        foreach(@SplittedLine) { if($_ =~ /^(.+?):(.+)$/ && $2 ne '-') { $ParamsHash->{$1} = $2; } }
+
+        # Skipすべきデータ <=> "値が-である" 
+        # Slipするべき要素をgrepで弾いて残りをmapしてるけど同じ正規表現2回ってどうも知性が足りないような……
+        my $ParamsHash = { map { $_ =~ /^(.+?):(.+)$/ && $1=>$2;} grep { $_ =~ /^(.+?):(.+)$/ && $2 ne '-'} @SplittedLine };
+
+        push @$ResultArray, Log->new(%$ParamsHash);
+    }
+
+    return $ResultArray;
 }
 
 1;
