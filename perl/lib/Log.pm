a46f0e9feb9ecb6891e5641af81634681e46e9d9
Asato Wakisaka <caaov627@hcn.zaq.ne.jp>
2013-07-30 22:40:42 +0900

    Q1とりあえずやっつけた

--- perl/lib/Log.pm
+++ perl/lib/Log.pm
@@ -8,18 +8,54 @@ sub new {
 }
 
 sub protocol {
+    my $self = shift;
+    return @{ $self->_split_req }[2];
 }
 
 sub method {
+    my $self = shift;
+    return @{ $self->_split_req }[0];
 }
 
 sub path {
+    my $self = shift;
+    return @{ $self->_split_req }[1];
 }
 
 sub uri {
+    my $self = shift;
+    my $req = $self->_split_req;
+
+#URI is constructed from (1)Protocol, (2)Host, (3)Path
+
+    #(1) Protocol 'HTTP/1.0' -> 'http'
+    my $protocol = lc([split('/',$req->[2])]->[0]);
+
+    #(2) Host
+    my $host = $self->{host};
+
+    #(3) Path
+    my $path = $req->[1];
+
+    return $protocol.'://'.$host.$path;
+
 }
 
 sub time {
+    my $self = shift;
+
+    # ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)
+    my ($sec,$min,$hour,$mday,$mon,$year) = gmtime $self->{epoch};
+
+    # YYYY-MM-DDThh:mm:ss
+    return sprintf("%04d-%02d-%02dT%02d:%02d:%02d",$year+1900,$mon+1,$mday,$hour,$min,$sec);
+}
+
+sub _split_req {
+    my $self = shift;
+
+    # ['GET', '/apache_pb.gif', 'HTTP/1.0']
+    return [split(' ',$self->{req})];
 }
 
 1;
